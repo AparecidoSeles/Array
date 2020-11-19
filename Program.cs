4475dc53ef8a456569fa8c799d005eddf436d515
junior <you@example.com>
2020-11-19 11:36:02 -0300

    Array

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array");
+
+            string[] nomes = new string[3];
+            int[] idades = new int[3];
+
+            for (var contador = 0; contador < 3 ; contador ++)
+            {
+                Console.WriteLine($"Digite o \n {contador+1}º nome");
+                nomes[contador] = Console.ReadLine();
+                Console.WriteLine("Digite a idade");
+                idades[contador] = int.Parse(Console.ReadLine());
+
+            }
+            
+            Console.WriteLine("Nomes cadastrados");
+
+            for (var contador = 0; contador <3; contador++)
+            {
+                
+                Console.WriteLine("Nome : "+nomes[contador]+ "\n Idade : "+idades[contador]);
+              
+            }
+        }
+    }
+}
